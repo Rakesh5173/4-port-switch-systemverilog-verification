73f6d4e0bb2e59714e58e8bbf5a08891defc21da
RAKESH <68835063+Rakesh5173@users.noreply.github.com>
2023-07-20 13:54:47 -0700

    Update packet_data.sv

--- packet_data.sv
+++ packet_data.sv
@@ -1,21 +1,10 @@
-/*-----------------------------------------------------------------
-File name     : packet_data.sv
-Developers    : Brian Dickinson
-Created       : 01/08/19
-Description   : lab1 packet data item 
-Notes         : From the Cadence "Essential SystemVerilog for UVM" training
--------------------------------------------------------------------
-Copyright Cadence Design Systems (c)2019
------------------------------------------------------------------*/
-
-// Follow instructions in lab book
 typedef enum {HEX,BIN,DEC} pp_t
-typedef enum {ANY,SINGLE,MULTICAST,BROADCAST} ptype_t// add print and type policies here
+typedef enum {ANY,SINGLE,MULTICAST,BROADCAST} ptype_t
 //typedef enum {UNIDED, IDED} tag_mode
 // packet class
 class packet;
 
-  // add properties here
+  // properties
 	local string name;
 	rand bit [3:0] target;
 	     bit [3:0] source;
@@ -54,7 +43,7 @@ class packet;
 	
 
 
-  // add constructor to set instance name and source by arguments and packet type
+  // constructor to set instance name and source by arguments and packet type
 
  // add print with policy
 
